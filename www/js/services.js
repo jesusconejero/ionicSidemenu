03215c33421a9a62182e07359e57e3833ea14d20
guadaltel <guadaltel@usuario4.guadaltel.es>
2014-07-16 19:53:34 +0200

    noooo

--- www/js/services.js
+++ www/js/services.js
@@ -12,6 +12,7 @@ angular.module('starter.services', [])
     { id: 1, name: 'G.I. Joe' },
     { id: 2, name: 'Miss Frizzle' },
     { id: 3, name: 'Ash Ketchum' }
+    { id: 4, name: 'CONEHOUSE' }
   ];
 
   return {
