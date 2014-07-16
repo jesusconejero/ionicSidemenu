7a5ac4aba6ea9f114c6322ee193d5760477b9ff6
guadaltel <guadaltel@usuario4.guadaltel.es>
2014-07-16 19:23:20 +0200

    Nuevos componentes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,58 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
+    function($scope, $cordovaCamera){
+        module.controller('PictureCtrl', function($scope, $cordovaCamera) {
+
+  $scope.takePicture = function() {
+    var options = { 
+        quality : 75, 
+        destinationType : Camera.DestinationType.DATA_URL, 
+        sourceType : Camera.PictureSourceType.CAMERA, 
+        allowEdit : true,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 100,
+        targetHeight: 100,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false
+    };
+
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+      alert("Que wena Conejo!");
+    }, function(err) {
+      alert("Mal Conejo, Mal!s");
+    });
+  }
+});
+
+    }
+    
+    
+    function($scope, $cordovaBarcodeScanner){
+        module.controller('BarcodeScannerCtrl', function($scope, $cordovaBarcodeScanner) {
+
+  $scope.scanBarcode = function() {
+    $cordovaBarcodeScanner.scan().then(function(imageData) {
+      alert("Que wena Conejo!");
+
+    }, function(err) {
+      alert("Mal Conejo, Mal!s");
+
+    });
+  };
+
+  // NOTE: encoding not functioning yet
+  $scope.encodeData = function() {
+    $cordovaBarcodeScanner.encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com").then(function(success) {
+      // Success! 
+    }, function(err) {
+      // An error occured. Show a message to the user
+
+    });      
+  }
+});
+
+    }
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
